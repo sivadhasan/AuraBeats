9afb6a9b067f17653fd66ed348df44a1bd280d85
Sivadhasan A <147160251+sivadhasan@users.noreply.github.com>
2025-04-07 23:36:29 +0530

    Update firebaseconfig.js

--- src/backend/firebaseconfig.js
+++ src/backend/firebaseconfig.js
@@ -10,14 +10,7 @@ import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 // Your web app's Firebase configuration
-const firebaseConfig = {
-  apiKey: "AIzaSyCXM-zlaocnMloxmivu3xBP16ypIvUbV-s",
-  authDomain: "aurabeats-27.firebaseapp.com",
-  projectId: "aurabeats-27",
-  storageBucket: "aurabeats-27.firebasestorage.app",
-  messagingSenderId: "1071557769060",
-  appId: "1:1071557769060:web:27641316712dc8fa9aac71"
-};
+
 
 // Initialize Firebase
 const firebaseApp = initializeApp(firebaseConfig);
